7d61b943e91fe6ae89d2caa35c4204cb8f2d650b
学習用 <@192.168.2.103>
2017-03-07 13:56:42 +0900

    番組の詳細情報取得機能追加

--- index.php
+++ index.php
@@ -2,27 +2,121 @@
 
 require "settings.php";
 require "ftp.php";
+require "mp3file.php";
 
 //一時間までの実行を許可
 ini_set("max_execution_time",3600);
 
-//FTPクライアント呼び出し
-$ftp = new FtpTransmission();
+// //FTPクライアント呼び出し
+// $ftp = new FtpTransmission();
 
-//ファイルのダウンロード
-if($ftp->connect(HOST, USER, PASS)){
-	$cue = getDownloadList("/",$ftp,SAVEDIR);
-	var_dump($cue);
-	foreach ($cue as $from){
-		$ftp->download($from,SAVEDIR);
-	}
-	$ftp->close();
-}
+// //ファイルのダウンロード
+// if($ftp->connect(HOST, USER, PASS)){
+// 	$cue = getDownloadList("/",$ftp,SAVEDIR);
+// 	var_dump($cue);
+// 	foreach ($cue as $from){
+// 		$ftp->download($from,SAVEDIR);
+// 	}
+// 	$ftp->close();
+// }
+
 //ファイルからポッドキャスト用リストを作成
+static $medialist = [];
+if ($dir = opendir(SAVEDIR)) {
+	while (($file = readdir($dir)) !== false) {
+		if ($file != "." && $file != "..") {
+			echo "$file\n";
+			//ファイル名からデータをパース
+			$local = (preg_match('/(\\\\|\/)$/', SAVEDIR)) ? SAVEDIR . basename($file) : SAVEDIR . '/' . basename($file);
+			$names = pathinfo($local);
 
+			//MP3ファイル名から詳細情報を取得
+			$mp3file = new MP3File($local);
+
+			$data = [];
+			$data['member'] = getDJCode($names["filename"]);
+
+			$data['title'] = (getTitleByDJC($data['member'])) ? getTitleByDJC($data['member']) ." ". preg_replace('/[^0-9]/', '', $names['filename']): $names['filename'];
+			$data['filesize'] = filesize($local);
+			$data['duration'] = MP3File::formatTime($mp3file -> getDuration(true));
+			$data['artist'] = getArtistNameByDJC($data['member']);
+			$data['picture'] = getPictureUrlByDJC($data['member']);
+			$data['date'] = date(DateTime::RFC822, filemtime($local));
+			$medialist[] = $data;
+		}
+	}
+	var_dump($medialist);
+	closedir($dir);
+}
 
 //xmlとして保存
 
+
+function getDJCode($title){
+	$member = 0;
+	if(stripos($title,'steve') !== false){
+		$member += 1;
+	}
+	if(stripos($title,'daisuke') !== false){
+		$member += 2;
+	}
+	if(stripos($title,'tox') !== false){
+		$member += 4;
+	}
+	if(stripos($title,'hiro') !== false){
+		$member += 8;
+	}
+	return $member;
+}
+function getTitleByDJC($member){
+	switch($member){
+		case 1:
+			return 'Steve Rock ＆ Pop Show';
+			break;
+		case 2:
+			return 'Daisuke Hip Hop Show';
+			break;
+		case 4:
+			return 'Tox Reggae Show';
+			break;
+		case 8:
+			return 'Hiro Chill Out Show';
+			break;
+		case 3:
+			return 'Steve ＆ Daisuke Show';
+			break;
+		default:
+			return false;
+	}
+}
+function getPictureUrlByDJC($member){
+	return 'https://i1.wp.com/ssr990.com/wp-content/uploads/2016/12/itunes_logo.jpg?ssl=1';
+}
+function getArtistNameByDJC($member){
+	switch($member){
+		case 1:
+			return 'DJ Steve';
+		break;
+		case 2:
+			return 'DJ Daisuke';
+		break;
+		case 4:
+			return 'DJ Tox';
+		break;
+		case 8:
+			return 'DJ Hiro';
+		break;
+		case 3:
+			return 'Steve ＆ Daisuke';
+		break;
+		default:
+			if($member > 0){
+				return 'Various Artists';
+			}else{
+				return 'Unknown Artist';
+			}
+	}
+}
 
 
 function getDownloadList($path,$client,$store){
@@ -41,8 +135,4 @@ function getDownloadList($path,$client,$store){
 		}
 	}
 	return $dlCue;
-}
-
-function bulkDownload(){
-
 }
