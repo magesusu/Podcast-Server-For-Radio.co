caeeff8cfed8042eaeb7acabd1d37bab6de63067
学習用 <@192.168.2.103>
2017-03-12 22:25:47 +0900

    Add configuration example

---
+++ settings.php
@@ -0,0 +1,25 @@
+<?php
+
+//Radio.co account settings
+const COUSER = 'myradiocoaccount@example.com';
+const COPASS = 'myradiocopassword';
+
+//radio.co FTP settings
+const HOST = 'purple.radio.co';
+const USER = 'sXXXXXXXXX.uXXXXXXXXX';
+const PASS = 'XXXXXXXXXXXX';
+
+//Media storage location (Do not forget the last "/")
+const SAVEDIR = '/home/example/example.com/public_html/listen/media/';
+//Client access URL
+const PREURL = 'https://listen.example.com/media/';
+
+
+//Podcast RSS save location (File name)
+const RSSPATH = '/home/example/example.com/public_html/listen/podcast.xml';
+
+//Header and footer to make Podcast RSS
+const HEADER = "/home/example/example.com/script/podcast_ftp/src/header.txt";
+const FOOTER = "/home/example/example.com/script/podcast_ftp/src/footer.txt";
+
+
